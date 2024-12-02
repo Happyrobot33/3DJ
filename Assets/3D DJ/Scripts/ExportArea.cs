177a36c653cf64b9ac7a4536444d2ed17272fb47
Happyrobot33 <matthewherber@sbcglobal.net>
2024-12-02 10:47:14 -0500

    Implement system to track all bones during record
    This prevents the dj from easily getting cutout when laying down or crouching. Also fixes the bug with playspace mover

---
+++ Assets/3D DJ/Scripts/ExportArea.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace com.happyrobot33.holographicreprojector
+{
+    public class ExportArea : UdonSharpBehaviour
+    {
+        public Camera cam;
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+                cam.enabled = true;
+            }
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+                cam.enabled = false;
+            }
+        }
+    }
+}
