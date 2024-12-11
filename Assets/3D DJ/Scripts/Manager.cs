495311d28a1929666b115476e281166b66a83f6b
Happyrobot33 <matthewherber@sbcglobal.net>
2024-12-11 09:38:36 -0500

    Work on UI changes

--- Assets/3D DJ/Scripts/Manager.cs
+++ Assets/3D DJ/Scripts/Manager.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,11 +12,10 @@ namespace com.happyrobot33.holographicreprojector
     using UnityEngine.Rendering.PostProcessing;
     using VRC.SDK3.Data;
 
-
     [AttributeUsage(AttributeTargets.Field)]
     public class DeveloperOnly : PropertyAttribute { }
 
-    public enum Mode
+    public enum Source
     {
         Record,
         Playback
@@ -42,13 +40,18 @@ namespace com.happyrobot33.holographicreprojector
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class Manager : UdonSharpBehaviour
     {
+        public const string MANAGERNAME = "3DJManager";
+
         [DeveloperOnly]
         public GameObject Recorder;
+
         [DeveloperOnly]
         public Material DataInput;
+
         [DeveloperOnly]
-        public Material[] playbackMaterials;
+        public GameObject mainPlaybackCube;
         public RenderTexture VideoTexture;
+
         [DeveloperOnly]
         public string worldID;
 
@@ -58,8 +61,10 @@ namespace com.happyrobot33.holographicreprojector
         public Anchor ColorAnchor;
         public Vector2Int ColorTextureSize;
         public Vector2Int ColorUVPosition;
+
         [DeveloperOnly]
         public Material ColorExtractMaterial;
+
         [DeveloperOnly]
         public CustomRenderTexture ColorExtractTexture;
 
@@ -69,8 +74,10 @@ namespace com.happyrobot33.holographicreprojector
         public Anchor DepthAnchor;
         public Vector2Int DepthTextureSize;
         public Vector2Int DepthUVPosition;
+
         [DeveloperOnly]
         public Material DepthExtractMaterial;
+
         [DeveloperOnly]
         public CustomRenderTexture DepthExtractTexture;
 
@@ -80,8 +87,10 @@ namespace com.happyrobot33.holographicreprojector
         public Anchor DataAnchor;
         public Vector2Int DataTextureSize;
         public Vector2Int DataUVPosition;
+
         [DeveloperOnly]
         public Material DataExtractMaterial;
+
         [DeveloperOnly]
         public CustomRenderTexture DataExtractTexture;
 
@@ -90,31 +99,36 @@ namespace com.happyrobot33.holographicreprojector
         /// </summary>
         [DeveloperOnly]
         public CustomRenderTexture RecordTexture;
-        [DeveloperOnly]
-        public Slider OffsetSlider;
-        [DeveloperOnly]
-        public TMP_Dropdown PlayerDropdown;
 
         private VRCPlayerApi playerToRecord;
         private Camera[] Cameras;
-        private Mode mode = Mode.Playback;
+        private Source mode = Source.Playback;
+        public float playerHeadOffset = 0.0f;
 
         #region Inspector Variables
         [Header("Preview Textures")]
         public Texture PreviewLayoutTexture;
+
         [DeveloperOnly]
         public bool DeveloperMode;
+
         [DeveloperOnly]
         public Areas CurrentlyEditingArea;
         #endregion
         void Start()
         {
+            //self tag
+            gameObject.name = MANAGERNAME;
+
+            //make our playback cube always at 0,0,0
+            mainPlaybackCube.transform.position = Vector3.zero;
+
             playerToRecord = Networking.LocalPlayer;
 
             //get all the children cameras of the recorder
             Cameras = Recorder.GetComponentsInChildren<Camera>();
 
-            SwitchSourceToPlayback();
+            SetSource(Source.Playback);
 
             //make sure the recorder is off
             Recorder.SetActive(false);
@@ -126,9 +140,18 @@ namespace com.happyrobot33.holographicreprojector
         public void SetupGlobalTextures()
         {
             //initialize the global textures
-            VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_Udon_3DJ_Color"), ColorExtractTexture);
-            VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_Udon_3DJ_Depth"), DepthExtractTexture);
-            VRCShader.SetGlobalTexture(VRCShader.PropertyToID("_Udon_3DJ_Data"), DataExtractTexture);
+            VRCShader.SetGlobalTexture(
+                VRCShader.PropertyToID("_Udon_3DJ_Color"),
+                ColorExtractTexture
+            );
+            VRCShader.SetGlobalTexture(
+                VRCShader.PropertyToID("_Udon_3DJ_Depth"),
+                DepthExtractTexture
+            );
+            VRCShader.SetGlobalTexture(
+                VRCShader.PropertyToID("_Udon_3DJ_Data"),
+                DataExtractTexture
+            );
 
             //convert to float array
             float[] worldIDArray = new float[32];
@@ -152,7 +175,6 @@ namespace com.happyrobot33.holographicreprojector
             }
             VRCShader.SetGlobalFloatArray(VRCShader.PropertyToID("_Udon_WorldID"), worldIDArray);
         }
-
 
         public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
         {
@@ -201,10 +223,13 @@ namespace com.happyrobot33.holographicreprojector
             minBound = new Vector3(minX, minY, minZ);
             maxBound = new Vector3(maxX, maxY, maxZ);
 
-
             //since we can only render a cube shape, make all side lengths equal to the longest side, while keeping the center the same
             float longestSide = Math.Max(maxX - minX, Math.Max(maxY - minY, maxZ - minZ));
-            centerBound = new Vector3((maxBound.x + minBound.x) / 2, (maxBound.y + minBound.y) / 2, (maxBound.z + minBound.z) / 2);
+            centerBound = new Vector3(
+                (maxBound.x + minBound.x) / 2,
+                (maxBound.y + minBound.y) / 2,
+                (maxBound.z + minBound.z) / 2
+            );
             minBound = centerBound - new Vector3(longestSide / 2, longestSide / 2, longestSide / 2);
             maxBound = centerBound + new Vector3(longestSide / 2, longestSide / 2, longestSide / 2);
         }
@@ -221,7 +246,11 @@ namespace com.happyrobot33.holographicreprojector
             Vector3 playerRot = player.GetRotation().eulerAngles;
 
             //undo the rotation of the player
-            bonePos = RotatePointAroundPivot(bonePos, playerPos, new Vector3(0, -playerRot.y - 45, 0));
+            bonePos = RotatePointAroundPivot(
+                bonePos,
+                playerPos,
+                new Vector3(0, -playerRot.y - 45, 0)
+            );
 
             return bonePos;
         }
@@ -235,9 +264,10 @@ namespace com.happyrobot33.holographicreprojector
 
             return input * percentage;
         }
+
         void Update()
         {
-            if (mode == Mode.Record)
+            if (mode == Source.Record)
             {
                 //get the player position
                 Vector3 playerPos = playerToRecord.GetPosition();
@@ -248,11 +278,19 @@ namespace com.happyrobot33.holographicreprojector
                 FromPoints(points, remapToScaledPlayer(0.125f));
 
                 //move the recorder to the player position rotated around the player
-                Vector3 recordCenter = RotatePointAroundPivot(centerBound, playerPos, new Vector3(0, playerToRecord.GetRotation().eulerAngles.y + 45, 0));
+                Vector3 recordCenter = RotatePointAroundPivot(
+                    centerBound,
+                    playerPos,
+                    new Vector3(0, playerToRecord.GetRotation().eulerAngles.y + 45, 0)
+                );
                 Recorder.transform.position = recordCenter;
 
                 //set the rotation to the player rotation + 45 on the y axis
-                Recorder.transform.rotation = Quaternion.Euler(0, playerToRecord.GetRotation().eulerAngles.y + 45, 0);
+                Recorder.transform.rotation = Quaternion.Euler(
+                    0,
+                    playerToRecord.GetRotation().eulerAngles.y + 45,
+                    0
+                );
 
                 //determine the scale by getting the distance between the player and the head on the y axis
                 float scale = (maxBound.y - minBound.y) / 2;
@@ -280,8 +318,13 @@ namespace com.happyrobot33.holographicreprojector
             //encapsulate some positions
             //build up a list of points
             DataList points = new DataList();
-            points.Add(new DataToken(rotationNullifiedBonePosition(HumanBodyBones.Head, playerToRecord) + new Vector3(0, remapToScaledPlayer(OffsetSlider.value), 0)));
-            
+            points.Add(
+                new DataToken(
+                    rotationNullifiedBonePosition(HumanBodyBones.Head, playerToRecord)
+                        + new Vector3(0, remapToScaledPlayer(playerHeadOffset), 0)
+                )
+            );
+
             for (int i = 0; i < (int)HumanBodyBones.LastBone; i++)
             {
                 //skip the head
@@ -296,9 +339,11 @@ namespace com.happyrobot33.holographicreprojector
                     continue;
                 }
 
-                points.Add(new DataToken(rotationNullifiedBonePosition((HumanBodyBones)i, playerToRecord)));
+                points.Add(
+                    new DataToken(rotationNullifiedBonePosition((HumanBodyBones)i, playerToRecord))
+                );
             }
-            
+
             //convert the list to an array
             DataToken[] tokens = points.ToArray();
             Vector3[] positions = new Vector3[tokens.Length];
@@ -310,52 +355,42 @@ namespace com.happyrobot33.holographicreprojector
             return positions;
         }
 
-
-        public void ToggleRecordSystem()
+        public void SetSource(Source source)
         {
-            Recorder.SetActive(!Recorder.activeSelf);
-
-            if (Recorder.activeSelf)
+            mode = source;
+            switch (source)
             {
-                mode = Mode.Record;
+                case Source.Playback:
+                    SetupRenderTextureExtractionZones(VideoTexture);
+                    Recorder.SetActive(false);
+                    break;
+                case Source.Record:
+                    SetupRenderTextureExtractionZones(RecordTexture);
+                    Recorder.SetActive(true);
+                    break;
             }
-            else
-            {
-                mode = Mode.Playback;
-            }
-        }
-
-        public void ToggleHolographic()
-        {
-            //toggle the holographic effect
-            const string keyword = "_HoloAffect";
-
-            foreach (Material mat in playbackMaterials)
-            {
-                mat.SetFloat(keyword, mat.GetFloat(keyword) == 0 ? 1 : 0);
-            }
-        }
-
-        public void SwitchSourceToPlayback()
-        {
-            SetupRenderTextureExtractionZones(VideoTexture);
-            //disable the recorder
-            //Recorder.SetActive(false);
-            //mode = Mode.Playback;
         }
 
         public void SetupRenderTextureExtractionZones(Texture source = null)
         {
-            SetupSource(ColorExtractMaterial, CalculateTopLeftUV(this, ColorAnchor, ColorUVPosition, ColorTexture), new Vector2Int(ColorTexture.width, ColorTexture.height), source);
-            SetupSource(DepthExtractMaterial, CalculateTopLeftUV(this, DepthAnchor, DepthUVPosition, DepthTexture), new Vector2Int(DepthTexture.width, DepthTexture.height), source);
-            SetupSource(DataExtractMaterial, CalculateTopLeftUV(this, DataAnchor, DataUVPosition, DataTexture), new Vector2Int(DataTexture.width, DataTexture.height), source);
-
-        }
-
-        public void SwitchSourceToRecord()
-        {
-            //Recorder.SetActive(true);
-            SetupRenderTextureExtractionZones(RecordTexture);
+            SetupSource(
+                ColorExtractMaterial,
+                CalculateTopLeftUV(this, ColorAnchor, ColorUVPosition, ColorTexture),
+                new Vector2Int(ColorTexture.width, ColorTexture.height),
+                source
+            );
+            SetupSource(
+                DepthExtractMaterial,
+                CalculateTopLeftUV(this, DepthAnchor, DepthUVPosition, DepthTexture),
+                new Vector2Int(DepthTexture.width, DepthTexture.height),
+                source
+            );
+            SetupSource(
+                DataExtractMaterial,
+                CalculateTopLeftUV(this, DataAnchor, DataUVPosition, DataTexture),
+                new Vector2Int(DataTexture.width, DataTexture.height),
+                source
+            );
         }
 
         public void EnforceCameraAspectRatio()
@@ -368,102 +403,34 @@ namespace com.happyrobot33.holographicreprojector
             }
         }
 
-        public void PlayerDropdownUpdate()
+        public void ChangePlayer(VRCPlayerApi player)
         {
-            //get the player index
-            int playerIndex = PlayerDropdown.value;
-
-            //get the player list
-            VRCPlayerApi[] players = VRCPlayerApi.GetPlayers(new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()]);
-
-            //get the player
-            playerToRecord = players[playerIndex];
-        }
-
-        public override void OnPlayerJoined(VRCPlayerApi player)
-        {
-            //clear
-            PlayerDropdown.ClearOptions();
-
-            //rebuild the list
-            VRCPlayerApi[] players = VRCPlayerApi.GetPlayers(new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()]);
-            TMP_Dropdown.OptionData[] options = new TMP_Dropdown.OptionData[players.Length];
-            for (int i = 0; i < players.Length; i++)
-            {
-                options[i] = new TMP_Dropdown.OptionData(players[i].displayName);
-            }
-
-            PlayerDropdown.AddOptions(options);
-
-            CheckDropdownSelection(players);
+            //reset the player offset height
+            playerHeadOffset = 0f;
+            playerToRecord = player;
         }
 
         public override void OnPlayerLeft(VRCPlayerApi player)
         {
-            //REMEMBER, The player leaving is STILL in the player list, so we need to remove them inside here
-            //clear
-            PlayerDropdown.ClearOptions();
-
-            //rebuild the list
-            VRCPlayerApi[] players = VRCPlayerApi.GetPlayers(new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()]);
-
-            //create a new list with the player removed
-            VRCPlayerApi[] realPlayersList = new VRCPlayerApi[players.Length - 1];
-            for (int i = 0, j = 0; i < players.Length; i++)
-            {
-                if (players[i] == player)
-                {
-                    continue;
-                }
-                realPlayersList[j] = players[i];
-                j++;
-            }
-            players = realPlayersList;
-
-            TMP_Dropdown.OptionData[] options = new TMP_Dropdown.OptionData[players.Length];
-            for (int i = 0; i < players.Length; i++)
-            {
-                options[i] = new TMP_Dropdown.OptionData(players[i].displayName);
-            }
-
-            PlayerDropdown.AddOptions(options);
-
-            //CheckDropdownSelection(players);
-            PlayerDropdown.SetValueWithoutNotify(0);
-
             if (player == playerToRecord)
             {
-                playerToRecord = players[0];
+                //set to local player
+                ChangePlayer(Networking.LocalPlayer);
             }
         }
 
-        /// <summary>
-        /// Check the dropdown selection and re-select the player that was previously selected
-        /// </summary>
-        private void CheckDropdownSelection(VRCPlayerApi[] players)
-        {
-            //re-select the player that was previously selected
-            bool broke = false;
-            for (int i = 0; i < players.Length; i++)
-            {
-                if (players[i].displayName == playerToRecord.displayName)
-                {
-                    PlayerDropdown.value = i;
-                    broke = true;
-                    break;
-                }
-            }
-            //if (!broke)
-            //{
-            //    playerToRecord = players[0];
-            // }
-        }
-
-
-        private void SetupSource(Material extractor, Vector2Int topLeft, Vector2Int size, Texture source = null)
+        private void SetupSource(
+            Material extractor,
+            Vector2Int topLeft,
+            Vector2Int size,
+            Texture source = null
+        )
         {
             //setup the source switcher to be based on the uv position info
-            Vector2 center = new Vector2(topLeft.x + (float)size.x / 2, topLeft.y + (float)size.y / 2);
+            Vector2 center = new Vector2(
+                topLeft.x + (float)size.x / 2,
+                topLeft.y + (float)size.y / 2
+            );
             extractor.SetVector("_Center", center);
             extractor.SetVector("_Size", new Vector2(size.x, size.y));
             if (source != null)
@@ -475,7 +442,12 @@ namespace com.happyrobot33.holographicreprojector
         /// <summary>
         /// Calculate the top left UV position based on the anchor
         /// </summary>
-        internal static Vector2Int CalculateTopLeftUV(Manager manager, Anchor anchor, Vector2Int position, Texture texture)
+        internal static Vector2Int CalculateTopLeftUV(
+            Manager manager,
+            Anchor anchor,
+            Vector2Int position,
+            Texture texture
+        )
         {
             Vector2Int topLeft = Vector2Int.zero;
 
@@ -485,13 +457,22 @@ namespace com.happyrobot33.holographicreprojector
                     topLeft = new Vector2Int(position.x, position.y);
                     break;
                 case Anchor.TopRight:
-                    topLeft = new Vector2Int(manager.VideoTexture.width - texture.width - position.x, position.y);
+                    topLeft = new Vector2Int(
+                        manager.VideoTexture.width - texture.width - position.x,
+                        position.y
+                    );
                     break;
                 case Anchor.BottomLeft:
-                    topLeft = new Vector2Int(position.x, manager.VideoTexture.height - texture.height - position.y);
+                    topLeft = new Vector2Int(
+                        position.x,
+                        manager.VideoTexture.height - texture.height - position.y
+                    );
                     break;
                 case Anchor.BottomRight:
-                    topLeft = new Vector2Int(manager.VideoTexture.width - texture.width - position.x, manager.VideoTexture.height - texture.height - position.y);
+                    topLeft = new Vector2Int(
+                        manager.VideoTexture.width - texture.width - position.x,
+                        manager.VideoTexture.height - texture.height - position.y
+                    );
                     break;
             }
 
---
+++ Assets/3D DJ/Scripts/UIManager.cs
@@ -0,0 +1,90 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+using static TMPro.TMP_Dropdown;
+using VRC.SDK3.Components;
+
+namespace com.happyrobot33.holographicreprojector
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class UIManager : UdonSharpBehaviour
+    {
+        private Manager manager;
+        public Slider slider;
+        public TMP_Dropdown dropdown;
+        public TMP_Dropdown sourceDropdown;
+
+        void Start()
+        {
+            SendCustomEventDelayedFrames(nameof(DelayedSearch), 1);
+
+            //setup source dropdown
+            sourceDropdown.ClearOptions();
+            string[] options = new string[] { "Record", "Playback" };
+            sourceDropdown.AddOptions(options);
+            sourceDropdown.SetValueWithoutNotify(1);
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            PopulateDropdown();
+        }
+
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            //we delay 1 frame here so the player list is actually correct
+            SendCustomEventDelayedFrames(nameof(PopulateDropdown), 1);
+        }
+
+        private void PopulateDropdown()
+        {
+            //update the dropdown
+            dropdown.ClearOptions();
+            VRCPlayerApi[] players = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];
+            VRCPlayerApi.GetPlayers(players);
+            string[] options = new string[players.Length];
+            for (int i = 0; i < players.Length; i++)
+            {
+                options[i] = players[i].displayName;
+            }
+            dropdown.AddOptions(options);
+
+            dropdown.SetValueWithoutNotify(0);
+        }
+
+        public void DelayedSearch()
+        {
+            manager = GameObject.Find(Manager.MANAGERNAME).GetComponent<Manager>();
+        }
+
+        public void SliderUpdated()
+        {
+            manager.playerHeadOffset = slider.value;
+        }
+
+        public void PlayerDropdownUpdated()
+        {
+            //find the player with that name
+            VRCPlayerApi[] players = new VRCPlayerApi[VRCPlayerApi.GetPlayerCount()];
+            VRCPlayerApi.GetPlayers(players);
+
+            manager.ChangePlayer(players[dropdown.value]);
+        }
+
+        public void SourceUpdated()
+        {
+            switch (sourceDropdown.value)
+            {
+                case 0:
+                    manager.SetSource(Source.Record);
+                    break;
+                case 1:
+                    manager.SetSource(Source.Playback);
+                    break;
+            }
+        }
+    }
+}
